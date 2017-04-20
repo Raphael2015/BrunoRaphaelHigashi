53c6512090d6f70e680ecb6f99a8d9665c68b5fd
Bruno  Juliano Fernandes Martini <brunojfmartini@brunos-mac.local>
2017-04-20 18:12:54 -0300

    Alerta adicionar estado - Somente nome

--- BrunoRaphaelHigashi/StatesRegisterViewController.swift
+++ BrunoRaphaelHigashi/StatesRegisterViewController.swift
@@ -7,6 +7,13 @@
 //
 
 import UIKit
+import CoreData
+
+
+enum ProductAlertType {
+    case add
+    case edit
+}
 
 class StatesRegisterViewController: UIViewController {
 
@@ -14,12 +21,28 @@ class StatesRegisterViewController: UIViewController {
     @IBOutlet weak var tfIof: UITextField!
     @IBOutlet weak var tvStates: UITableView!
     
+    var dataSource: [States] = []
+    var state: States!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        loadStates()
         // Do any additional setup after loading the view.
+    }
+    
+    
+    func loadStates(){
+        
+        let fetchRequest: NSFetchRequest<States> = States.fetchRequest()
+        let sortDescriptor = NSSortDescriptor(key: "name", ascending: true)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        do{
+            dataSource = try context.fetch(fetchRequest)
+            tvStates.reloadData()
+        } catch {
+            print(error.localizedDescription)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -38,6 +61,33 @@ class StatesRegisterViewController: UIViewController {
     }
     */
     @IBAction func addStateData(_ sender: UIButton) {
+        showAlert(type: .add, state: nil)
+        
+    }
+    
+    
+    func showAlert(type: ProductAlertType, state: States?) {
+        let title = (type == .add) ? "Adicionar" : "Editar"
+        let alert = UIAlertController(title: "\(title) Estado", message: nil, preferredStyle: .alert)
+        alert.addTextField { (textField: UITextField) in
+            textField.placeholder = "Nome do estado"
+            if let name = state?.name {
+                textField.text = name
+            }
+        }
+        
+        alert.addAction(UIAlertAction(title: title, style: .default, handler: { (action: UIAlertAction) in
+            let state = state ?? States(context: self.context)
+            state.name = alert.textFields?.first?.text
+            do {
+                try self.context.save()
+                self.loadStates()
+            } catch {
+                print(error.localizedDescription)
+            }
+        }))
+        alert.addAction(UIAlertAction(title: "Cancelar", style: .cancel, handler: nil))
+        present(alert, animated: true, completion: nil)
     }
 
 }
