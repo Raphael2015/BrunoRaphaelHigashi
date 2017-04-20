a9e5a534ae6227b53d0a0340bc15212002554bf0
Bruno  Juliano Fernandes Martini <brunojfmartini@brunos-mac.local>
2017-04-20 17:31:15 -0300

    Core-Data - adicionando produtos na tabela

--- BrunoRaphaelHigashi/ProductResgisterViewController.swift
+++ BrunoRaphaelHigashi/ProductResgisterViewController.swift
@@ -5,22 +5,43 @@
 //  Created by Bruno  Juliano Fernandes Martini on 18/04/17.
 //  Copyright © 2017 Bruno  Juliano Fernandes Martini. All rights reserved.
 //
-
+import Foundation
 import UIKit
+import CoreData
+
 
 class ProductResgisterViewController: UIViewController {
+    
+    
 
     @IBOutlet weak var tfProdName: UITextField!
     @IBOutlet weak var ivProdPicture: UIImageView!
     @IBOutlet weak var tfStateProd: UITextField!
     @IBOutlet weak var tfProdPrice: UITextField!
     @IBOutlet weak var swProdCard: UISwitch!
+    @IBOutlet weak var btnAddUpdate: UIButton!
+    
+    
+    var product: Product!
+    var smallImage: UIImage!
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        if product != nil {
+            tfProdName.text = product.name
+            tfProdPrice.text = "\(product.usPrice)"
+            if let image = product.picture as? UIImage {
+                ivProdPicture.image = image
+            }
+            btnAddUpdate.setTitle("Atualizar" , for: .normal)
+            
+        }
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -37,10 +58,34 @@ class ProductResgisterViewController: UIViewController {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
+     
     */
+    
+    @IBAction func close(_ sender: UIButton?) {
+        dismiss(animated: true, completion: nil)
+    }
+    
     @IBAction func addState(_ sender: UIButton) {
     }
 
     @IBAction func addProduct(_ sender: UIButton) {
+        
+        if product == nil { product = Product(context: context)    }
+        product.name = tfProdName.text!
+        product.usPrice = Double(tfProdPrice.text!)!
+        if smallImage != nil {
+            product.picture = smallImage
+        }
+        do{
+            try context.save()
+        } catch {
+                print(error.localizedDescription)
+        }
+        
+        close(nil)
+        
     }
+    
+    
+    
 }
--- BrunoRaphaelHigashi/StatesRegisterViewController.swift
+++ BrunoRaphaelHigashi/StatesRegisterViewController.swift
@@ -15,6 +15,7 @@ class StatesRegisterViewController: UIViewController {
     @IBOutlet weak var tvStates: UITableView!
     
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
